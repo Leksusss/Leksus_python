7977ab5f7421627d00cff511d32a1a9fd9cd7d43
Leksusss <leksinaulya@gmail.com>
2024-12-13 15:01:34 +0300

    закончила PZ_4

--- PZ_4/PZ_4.1.py
+++ PZ_4/PZ_4.1.py
@@ -1,15 +1,21 @@
 # Дано вещественное число A и целое число N (>0). Найти A в степени N: AN = AA ...
 # •A (числа A перемножаются N раз).
-
-try:
-    A = int(input('Введите целое число A: '))
-    N = int(input('Введите целое число N, больше 0: '))
-    if N <= 0:
-        print('N должно быть больше 0.')
-    else:
-        result = A
-        for i in range(1, N):
-            result = result * A
-        print(A, ' в степени ', N, ' равно ', result)
-except ValueError:
-    print('Что-то пошло не так. Введите корректное число.')
+while True:
+    try:
+        A = int(input('Введите целое число A: '))
+        break
+    except ValueError:
+        print('Некорректно введено число А. Повторите попытку.')
+while True:
+    try:
+        N = int(input('Введите целое число N, больше 0: '))
+        if N <= 0:
+            print('N должно быть больше 0.')
+        else:
+            break
+    except ValueError:
+        print('Некорректно введено число N. Повторите попытку.')
+result = A
+for i in range(1, N):
+    result = result * A
+print(A, ' в степени ', N, ' равно ', result)
