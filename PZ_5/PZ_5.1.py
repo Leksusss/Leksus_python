93621a2ac5905d2be5930e6abc9fb3d5a728b405
Leksusss <leksinaulya@gmail.com>
2024-12-18 15:41:58 +0300

    закончила PZ_5

--- PZ_5/PZ_5.1.py
+++ PZ_5/PZ_5.1.py
@@ -1,11 +1,12 @@
 # Найти сумму чисел ряда 1,2,3,...,60 с использованием функции нахождения суммы.
 # Использовать локальные переменные.
 
-def sum_num(n):
-  itog = 0
-  for i in range(1, n + 1):
-    itog = itog + i
-  return itog
+def sum_numbers(n):
+    total = 0
+    for i in range(1, n + 1):
+        total = total + i
+    return total
 
-result = sum_num(60)
+
+result = sum_numbers(60)
 print("Сумма чисел от 1 до 60:", result)
