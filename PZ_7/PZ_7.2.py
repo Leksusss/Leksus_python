1029afd231d5e7ec25a3f8d07107ac1688109458
Leksusss <leksinaulya@gmail.com>
2025-02-07 12:26:24 +0300

    закончила PZ_7

---
+++ PZ_7/PZ_7.2.py
@@ -0,0 +1,29 @@
+# Дана строка-предложение на русском языке. Вывести самое длинное слово в
+# предложении. Если таких слов несколько, то вывести первое из них. Словом считать
+# набор символов, не содержащий пробелов, знаков препинания и ограниченный
+# пробелами, знаками препинания или началом/концом строки.
+
+
+while True:
+    stroka = input('Введите свою строку, состоящую только из слов: ')
+    if len(stroka) == 0:
+        print('Вы ничего не ввели. Повторите попытку')
+        continue
+
+    bez_probel = stroka.split()
+    proverka = True
+    for slovo in bez_probel:
+        if not slovo.isalpha():
+            proverka = False
+            print('Вы ввели не только слова. Повторите попытку')
+            break
+
+        if proverka:
+            longest_word = ""
+            for slovo in bez_probel:
+                if len(slovo) > len(longest_word):
+                    longest_word = slovo
+
+            print("Введена корректная строка:", stroka)
+            print("Самое длинное слово в нём:", longest_word)
+            break
