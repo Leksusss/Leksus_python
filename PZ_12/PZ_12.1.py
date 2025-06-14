ae8b2fb4a979c8ad8e2bd386b899cdc7578feead
Leksusss <leksinaulya@gmail.com>
2025-06-14 08:14:52 +0300

    закончила PZ_12

--- PZ_12/PZ_12.1.py
+++ PZ_12/PZ_12.1.py
@@ -2,18 +2,18 @@
 
 
 try:
-    numbers_input = input("Введите целые числа через пробел: ")
-    numbers = list(map(int, numbers_input.split()))
+    numbers = input("Введите целые числа через пробел: ")
+    N = list(map(int, numbers_input.split()))
 
     K = int(input("Введите число для поиска: "))
 
-    matches = list(map(lambda x: x == K, numbers))
+    matches = list(map(lambda x: x == K, N))
     found_Tr = True in matches
 
     if found_Tr:
-        print(f"Число {K} присутствует в списке {numbers}")
+        print(f"Число {K} присутствует в списке {N}")
     else:
-        print(f"Число {K} отсутствует в списке {numbers}")
+        print(f"Число {K} отсутствует в списке {N}")
 
 
 except ValueError:
