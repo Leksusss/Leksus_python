de766fc8c35efc3bc164b07a3199175c6df87509
Leksusss <leksinaulya@gmail.com>
2025-06-24 13:23:14 +0300

    закончила PZ_13

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -2,21 +2,17 @@
 
 
 import random
-matrix = [[random.randint(-100, 100) for i in range(4)] for i in range(4)]
+matrix = [[random.randint(-10, 10) for i in range(4)] for i in range(4)]
 
 print("Матрица: ")
 for row in matrix:
     print(row)
 
-max_element = 0
+all_elements = [num for row in matrix for num in row]
+proverka = list(filter(lambda x: x > 0 and x % 4 == 0, all_elements))
 
-for row in matrix:
-    for num in row:
-        if num > 0 and num % 4 == 0:
-            if num > max_element:
-                max_element = num
-
-if max_element > 0:
+if proverka:
+    max_element = max(proverka)
     print(f"\nМаксимальный положительный элемент, кратный 4: {max_element}")
 else:
     print("\nВ матрице нет положительных элементов, кратных 4.")
